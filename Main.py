36e6a99a7cbf8898fc414dd685ec7a172e9047a1
shravanthi_manohar <shravantimanohar@gmail.com>
2015-05-11 18:49:15 -0600

    Code Works!

--- Main.py
+++ Main.py
@@ -18,3 +18,61 @@ con = db.db_connect()
 #query1 = 'INSERT INTO student1 (Course_id,user_name,state,user_id)  VALUES ("C628944","Noah Thomas","active","U531649")'
 #print query1
 #con.cursor().execute(query1)
+
+for row in dlist:
+    #print row["user_id"]
+    keys = row.keys()
+    values = row.values()
+    #print values
+    columns = ', '.join(keys)                  # TODO Check the order
+    placeholder = "'{0}','{1}','{2}','{3}'".format(row[keys[0]],values[1],values[2],values[3])
+
+ 
+#TODO batch process the query result
+    query = "INSERT INTO student1 ('course_id','user_name','state','user_id') VALUES (%s);" % (placeholder)
+    #print query
+#==============================================================================
+#     try:
+#        con.cursor().executemany(query, values) 
+#        con.commit()
+#     except:
+#==============================================================================
+    db.execute_query(con,query)    
+    con.commit() 
+    result = con.cursor().execute("SELECT * FROM student1;") 
+print result.fetchall() 
+    
+res1= con.cursor().execute("SELECT course_id from course WHERE state is 'active';").fetchall()    
+print res1
+
+result_list = []
+
+#Then need to filter the list of active courses with active students 
+for c_id in res1:
+  #Course = str(c_id[0])
+  Course =  c_id[0]
+  
+  query = "SELECT user_name,course_id from student1 WHERE state = 'active' AND course_id='{0}';".format(Course)
+  #print query  
+  res2= con.cursor().execute(query).fetchall()    
+  result_list.append(res2) 
+
+print result_list 
+with open('test.txt', 'w') as f:
+    for row in result_list:
+        #print row
+        f.write("%s\n" % str(row))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
